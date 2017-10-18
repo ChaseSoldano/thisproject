5831848bad49e9bb8b505e837958323ec2225b54
beatacable <beatacable@gmail.com>
2017-10-18 14:55:15 -0500

    fixed the tree

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,23 @@
-let justin = 2; 
+const app = angular.module('myApp');
+    app.config(function($routeProvider){
+          $routeProvider
+            .when('/', {
+                  templateUrl: 'index.html'
+            })
+            .when('login.html', {
+                  templateUrl: 'login.html'
+            })
+          .otherwise({
+                redirectTo: '/'
+          })
+    })
+
+    app.controller('loginController', function($scope, $location){
+          $scope.submit = function(){
+              const uname = $scope.username;
+              const password = $scope.password;
+                  if($scope.username == 'admin' && $scope.password == 'admin') {
+                        $location.path('/index.html')
+                  }
+          }
+    });
