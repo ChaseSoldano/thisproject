5c7d498362561f1a2ba8f5ca96a1f9d23094ee40
chase soldano <chasesoldano@gmail.com>
2017-10-23 19:17:52 -0500

    added mocha in package.json dependencies and a DB config file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.use(express.static(__dirname + "/view"));
 app.use(express.static(__dirname + "/node_modules"));
+app.use(express.static(__dirname + "/controllers"));
 
 app.listen(3000);
 console.log("server runnng on port 3000");
