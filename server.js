5461a7d453e62fef6e10beed8ac3f0d0b8fd47b4
beatacable <beatacable@gmail.com>
2017-11-08 21:26:00 -0600

    fixed files on my end

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const path = require("path");
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const errorhandler = require("errorhandler");
-=======
-// const connect = require('connect');
-const errorhandler = require('errorhandler');
->>>>>>> 55af5e4ea3298c483cae828d3115c310b582687d
 
 // const app = connect();
 const app = express();
 
-<<<<<<< HEAD
+
 // app.set('port', 27017);
-=======
 if (process.env.NODE_ENV === 'development') {
   // only use in development
   app.use(errorhandler())
 };
 
 //app.set('port', 3000);
->>>>>>> 55af5e4ea3298c483cae828d3115c310b582687d
 
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json
@@ -29,7 +22,7 @@ app.use(bodyParser.json());
 
 
 //connecting to MongoDB
-mongoose.connect('mongodb://localhost/meanapp');
+//mongoose.connect('mongodb://localhost/meanapp');
 // new Mongo();
 // new Mongo("localhost");
 // new Mongo("localhost:27017");
@@ -55,12 +48,6 @@ app.get('/', function (req, res) {
 //   console.log('running on server ' + port);
 // });
 const port = 3000;
-<<<<<<< HEAD
- app.listen(port, ()=> {});
-console.log("server running on port 3000");
-=======
->>>>>>> 55af5e4ea3298c483cae828d3115c310b582687d
-
 app.listen(port, () => {
   console.log("server running on port "+port);
 });
