fbd0fac1a7b86c1f9b334fb9880fc234ee8a5ce8
thines23 <t.i.hines20@gmail.com>
2017-11-14 14:41:52 -0600

    update

--- Public/Resources/js/app.js
+++ Public/Resources/js/app.js
@@ -1,8 +1,17 @@
-angular.module( 'myApp', []);
-<<<<<<< HEAD
-angular.module('app', ['ngResource']);
-=======
+angular.module( 'myApp', ['ngResource', 'ngRoute']);
 
+// angular.module('app', ['ngResource']);
+
+// angular routing view pages.
+myApp.config(['$routeProvider', function('$routeProvider'){
+    $routeProvider
+    .when('/red', {
+        templateUrl: 'view/red.html'
+    })
+}])
+
+
+//end of angular routing.
 var express = require('express');
 var path = require('path');
 var favicon = require('server-favicon');
@@ -26,4 +35,48 @@ app.use(function(err, req, res, next) {
     res.json({"message": err.name +": " + err.message});
   }
 });
->>>>>>> 55af5e4ea3298c483cae828d3115c310b582687d
+// search box for the users to type in there location and also add.
+
+app.controller("searchBoxCtrl", function($scope){
+      $scope.eventFinder = [
+                  { 'names':'Infosys Technologies',
+                    'address': '400 Bagley St, Detroit, Michigan 48226',
+                     'price': 125000  },
+                    { 'names':'Cognizant Technologies',
+                      'employees': 100000,
+                      'headoffice': 'Bangalore'},
+                      { 'names':'Wipro',
+                        'employees': 115000,
+                        'headoffice': 'Bangalore'},
+                        { 'names':'Tata Consultancy Services (TCS)',
+                          'employees': 150000,
+                          'headoffice': 'Bangalore'},
+                          { 'names':'HCL Technologies',
+                            'employees': 90000,
+                            'headoffice': 'Noida'},
+
+
+      ];
+      $scope.addRow = function(){
+            	$scope.eventFinder.push({ 'names':$scope.names, 'events': $scope.events, 'price':$scope.price });
+            	$scope.names ='';
+            	$scope.events ='';
+            	$scope.price ='';
+            };
+
+     $scope.removeRow = function(names){
+            var remove = -1;//this remove variable is apart of if statment
+            var comArr = eval( $scope.eventFinder );// is needed in the For loop.
+
+            for ( var i = 0; i < comArr.length; i++ ) {
+               if ( comArr[i].names === names ) {
+                        remove = i;
+                        break;
+               }
+            }
+            if( comArr === -1 ){
+                alert( "Something gone wrong" );
+            }
+            $scope.eventFinder.splice( remove, 1 );
+     };
+});
