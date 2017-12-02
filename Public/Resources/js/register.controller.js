68caab173ff58b628708de919f1764c10beb2548
rherrera1215 <30609156+rherrera1215@users.noreply.github.com>
2017-12-01 18:14:56 -0600

    fixed css

---
+++ Public/Resources/js/register.controller.js
@@ -0,0 +1,38 @@
+module.export.register = function(req, res) {
+  var user = new User();
+
+  user.name = req.body.name;
+  user.email = req.body.email;
+
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    var token;
+    token = user.generateJwt();
+    res.status(200);
+    res.json({
+      "token": token
+    });
+  });
+};
+
+
+function registerCtrl($location, authentication) {
+  var vm = this;
+
+  vm.credentials = {
+    name: "",
+    email: "",
+    password: ""
+  };
+
+  vm.onSubmit = function() {
+    authentication
+     .register(vm.credentials)
+     .error(function(err) {
+       alert(err);
+     })
+     .then(function() {
+       $location.path('profile');
+     });
+  };
